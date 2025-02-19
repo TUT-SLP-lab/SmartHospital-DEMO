86ac099ad340eab2a84fe32dd7d0200b90938990
rikuto_ym <chick12032001@gmail.com>
2025-02-20 06:40:00 +0900

    fix API_KEY

--- src/lib/typechat/swipe.ts
+++ src/lib/typechat/swipe.ts
@@ -65,7 +65,7 @@ export interface SwipeFromChatGPT{
 
 const model = createLanguageModel({
   OPENAI_MODEL: 'gpt-4o',
-  OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 });
 
 export const swipeTranslator = createJsonTranslator<SwipeFromChatGPT>(
